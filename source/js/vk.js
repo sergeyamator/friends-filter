792272a8b92f85dec362dd76293f4180877a9e6d
Sergey Rubets <sramator@gmail.com>
2016-09-03 19:24:13 +0300

    Friends rendering was added

--- source/js/vk.js
+++ source/js/vk.js
@@ -1,15 +1,40 @@
 'use strict';
 
-VK.init({
-  apiId: 5586953
+let Handlebars = require('handlebars');
+
+new Promise(resolve => {
+  if (document.readyState === 'complete') {
+    resolve();
+  } else {
+    window.onload = resolve;
+  }
+}).then(() => {
+  return new Promise((resolve) => {
+    VK.init({
+      apiId: 5586953
+    });
+
+    VK.Auth.login(function(response) {
+      if (response.session) {
+        resolve(response);
+      } else {
+        console.log('не все ок');
+      }
+    }, 2);
+  })
+}).then(() => {
+  return new Promise((resolve, reject) => {
+    VK.api('friends.get',{'name_case' : 'nom', fields: 'nickname'}, response => {
+      let source = document.querySelector('.user-template').innerHTML,
+        templateFunction = Handlebars.compile(source),
+        template = templateFunction({data: response.response});
+
+      document.querySelector('.friends_list').innerHTML = template;
+      resolve();
+    })
+  });
 });
 
-let cb = response => {
-  if (response.session) {
-    console.log('все ок');
-  } else {
-    console.log('не все ок');
-  }
-};
 
-VK.Auth.login(cb);
+
+
